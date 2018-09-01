bbe0c2b9254740f8a933683822c3f5c843996125
eri.kato <kato-eri-hj@ynu.jp>
2018-09-01 23:24:40 +0900

    Add validation

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
+import models.validators.ContentValidator;
 import utils.DBUtil;
 
 /**
@@ -48,13 +51,27 @@ public class CreateServlet extends HttpServlet {
 		    t.setCreated_at(currentTime);
 		    t.setUpdated_at(currentTime);
 
-		    em.getTransaction().begin();
-		    em.persist(t);
-		    em.getTransaction().commit();
-		    request.getSession().setAttribute("flush", "登録が完了しました。");
-		    em.close();
+		    List<String> errors = ContentValidator.validate(t);
+		    if(errors.size() > 0) {
+		        em.close();
 
-		    response.sendRedirect(request.getContextPath()+"/index");
+		        request.setAttribute("_token", request.getSession().getId());
+		        request.setAttribute("content", content);
+		        request.setAttribute("errors", errors);
+
+		        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+		        rd.forward(request, response);
+
+		    } else {
+		        em.getTransaction().begin();
+	            em.persist(t);
+	            em.getTransaction().commit();
+	            request.getSession().setAttribute("flush", "登録が完了しました。");
+	            em.close();
+
+	            response.sendRedirect(request.getContextPath()+"/index");
+		    }
+
 		}
 	}
 
